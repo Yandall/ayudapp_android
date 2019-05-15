eeed2de4b3a9f687d9a1d891b66b3dfceba5decb
proyectoestudiantesudem <49171784+proyectoestudiantesudem@users.noreply.github.com>
2019-05-15 09:04:35 -0500

    Pantallas modulo registro

---
+++ app/src/main/java/com/example/appalertas2/PantallaAgregarContacto.java
@@ -0,0 +1,13 @@
+package com.example.appalertas2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaAgregarContacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_agregar_contacto);
+    }
+}
