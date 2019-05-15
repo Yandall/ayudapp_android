3741e5239639ed0c85fb0d8dc4530c9164f52c89
proyectoestudiantesudem <49171784+proyectoestudiantesudem@users.noreply.github.com>
2019-05-15 01:05:27 -0500

    commit 3 Servicio de sms (Sin terminar)

--- app/src/main/java/com/example/appalertas2/PantallaEmergenciaPrincipal.java
+++ app/src/main/java/com/example/appalertas2/PantallaEmergenciaPrincipal.java
@@ -12,8 +12,28 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class PantallaEmergenciaPrincipal extends AppCompatActivity {
     ImageButton btnEmergenciaPrincipal, btnMiPerfilPrincipal;
+    static String telefono_contacto;
+
 
 
     @Override
@@ -21,6 +41,7 @@ public class PantallaEmergenciaPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_emergencia_principal);
         conectar();
+        //recibirTelefonoContacto();
         btnMiPerfilPrincipal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,14 +65,59 @@ public class PantallaEmergenciaPrincipal extends AppCompatActivity {
             public void onClick(View v) {
 
                 enviarMensaje("3234729383","Sorry we");
-                //Toast.makeText(getApplicationContext(), "La ayuda va en camino", Toast.LENGTH_LONG).show();
-                //Intent i = new Intent(getApplicationContext(), PantallaEmergenciaPrincipal.class);
-                //startActivity(i);
+
             }
         });
 
 
     }
+    public void recibirTelefonoContacto(final String idUsuario){
+
+
+        String URL_recibirTelefono = "http://labs.ebotero.com/servicios_ayudapp/public/telefono_contacto_usuario";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_recibirTelefono,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+
+                            JSONObject jsonobj = new JSONObject(response);
+                            JSONArray list = jsonobj.optJSONArray("data");
+
+                            if(list.length() != 0) {
+                                telefono_contacto = jsonobj.getString("celular");
+                            } else
+                            {
+                                Toast.makeText(getApplicationContext(), "No tiene ningun contacto Registrado", Toast.LENGTH_LONG).show();
+                            }
+
+                        }catch (JSONException ex){
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<>();
+                parametros.put("IdUsuario", idUsuario);
+                return parametros;
+            }
+        };
+
+
+        RequestQueue requestqueue = Volley.newRequestQueue(this);
+        requestqueue.add(stringRequest);
+    }
+
+
+
 
     private void enviarMensaje (String numero, String mensaje){
         try{
